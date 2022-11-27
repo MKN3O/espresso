77a97877f091ccda18a417368b6e8237a4ea18ea
MKN3O <62808838+MKN3O@users.noreply.github.com>
2022-11-28 00:13:05 +0300

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-
-con = sqlite3.connect("coffee.sqlite")
-cur = con.cursor()
-cur.execute(f"""INSERT INTO coffee(sort, grade, condition, descriptor, price, mass)
-                             VALUES('Робуста','Тёмная','True','Табачный',
-                             '1000', '1000')""")
-con.commit()
